646441ffb72fd57a82f626384356d7fd52185b72
koreanguyyyy <park6049612@naver.com>
2025-06-10 13:09:21 +0900

    feat: ADD Order-Entity

--- src/main/java/com/nhnacademy/illuwa/domain/order/entity/OrderItem.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/entity/OrderItem.java
@@ -3,7 +3,6 @@ package com.nhnacademy.illuwa.domain.order.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
