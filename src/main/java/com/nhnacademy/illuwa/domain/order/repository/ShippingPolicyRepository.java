aa188de3735e915a21848639ad7034f8bd834ddc
koreanguyyyy <park6049612@naver.com>
2025-06-13 14:25:35 +0900

    feat: service 코드 작성

--- src/main/java/com/nhnacademy/illuwa/domain/order/repository/ShippingPolicyRepository.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/repository/ShippingPolicyRepository.java
@@ -2,12 +2,32 @@ package com.nhnacademy.illuwa.domain.order.repository;
 
 import com.nhnacademy.illuwa.domain.order.entity.ShippingPolicy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ShippingPolicyRepository extends JpaRepository<ShippingPolicy, Long> {
 
     // 전체 조회
     // findAll
 
+    // 활성화 상태로 정책 조회
+    List<ShippingPolicy> findByActive(boolean active);
+
     // id로 정책 조회하기
-    ShippingPolicy findByShippingPolicyId(long shippingPolicyId);
+    Optional<ShippingPolicy> findByShippingPolicyId(long shippingPolicyId);
+
+    /** 정책 옵션 삭제 (active 컬럼값 false 로 변경)
+     * @param shippingPolicyId 변경할 정책 ID
+     * @param active 활성화 여부
+     * @return 수정된 행 수
+    * */
+    @Modifying
+    @Transactional
+    @Query("update ShippingPolicy s set s.active = :active where s.shippingPolicyId = :shippingPolicyId")
+    int updateActiveByPackagingId(@Param("shippingPolicyId") long shippingPolicyId, @Param("active") Boolean active);
 }
