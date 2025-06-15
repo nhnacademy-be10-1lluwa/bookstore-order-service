8076d93b9b193994412ab840bef7314ea600ebe2
koreanguyyyy <park6049612@naver.com>
2025-06-15 20:04:53 +0900

    Refactor: 반품 엔티티 수정

--- src/main/java/com/nhnacademy/illuwa/domain/order/entity/ReturnRequest.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/entity/ReturnRequest.java
@@ -3,7 +3,10 @@ package com.nhnacademy.illuwa.domain.order.entity;
 import com.nhnacademy.illuwa.domain.order.entity.types.ReturnReason;
 import com.nhnacademy.illuwa.domain.order.entity.types.ReturnStatus;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
@@ -20,6 +23,9 @@ public class ReturnRequest {
     private long returnId;
 
     @Setter
+    private String memberId;
+
+    @Setter
     private ZonedDateTime requestedAt;
 
     @Setter
@@ -29,16 +35,20 @@ public class ReturnRequest {
     private BigDecimal shippingFeeDeducted;
 
     @Setter
+    @Enumerated(EnumType.STRING)
     private ReturnReason returnReason;
 
     @Setter
+    @Enumerated(EnumType.STRING)
     private ReturnStatus status;
 
     @OneToOne
+    @JoinColumn(name = "order_order_id", referencedColumnName = "order_id")
     private Order order;
 
     @Builder
-    public ReturnRequest(ZonedDateTime requestedAt, ZonedDateTime returnedAt, BigDecimal shippingFeeDeducted, ReturnReason returnReason, ReturnStatus status, Order order) {
+    public ReturnRequest(String memberId, ZonedDateTime requestedAt, ZonedDateTime returnedAt, BigDecimal shippingFeeDeducted, ReturnReason returnReason, ReturnStatus status, Order order) {
+        this.memberId = memberId;
         this.requestedAt = requestedAt;
         this.returnedAt = returnedAt;
         this.shippingFeeDeducted = shippingFeeDeducted;
