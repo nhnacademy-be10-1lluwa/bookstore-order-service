b2561dca2ea1388f1398bf113654027b4c9362d6
koreanguyyyy <park6049612@naver.com>
2025-06-18 13:03:00 +0900

    refactor: Valid 추가

--- src/main/java/com/nhnacademy/illuwa/domain/order/dto/order/OrderCreateRequestDto.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/dto/order/OrderCreateRequestDto.java
@@ -1,8 +1,14 @@
 package com.nhnacademy.illuwa.domain.order.dto.order;
 
+import com.nhnacademy.illuwa.domain.order.entity.Order;
 import com.nhnacademy.illuwa.domain.order.entity.OrderItem;
+import com.nhnacademy.illuwa.domain.order.entity.ShippingPolicy;
 import com.nhnacademy.illuwa.domain.order.entity.types.OrderStatus;
+import com.querydsl.core.annotations.QueryProjection;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,16 +18,19 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class OrderCreateRequestDto {
 
-    private long shippingPolicyId;
+    private Long memberId;
+
+    @NotNull
+    private Long shippingPolicyId;
+
+    @Size(min=1)
+    @NotNull
     private List<OrderItem> items;
-    private ZonedDateTime orderDate;
-    private ZonedDateTime deliveryDate;
-    private BigDecimal totalPrice;
-    private OrderStatus orderStatus;
-    private String password;
+
+    private ZonedDateTime requestedDeliveryDate;
+
 }
 
 // 프론트 -> order 서버 (주문 요청)
