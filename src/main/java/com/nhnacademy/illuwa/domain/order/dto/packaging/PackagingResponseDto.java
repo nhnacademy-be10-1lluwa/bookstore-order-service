82bbb17162e7313e45ff548340921a5ae9e829bf
koreanguyyyy <park6049612@naver.com>
2025-06-19 08:41:40 +0900

    refactor: PackagingResponseDto.java 쿼리 인젝션 추가

--- src/main/java/com/nhnacademy/illuwa/domain/order/dto/packaging/PackagingResponseDto.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/dto/packaging/PackagingResponseDto.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.illuwa.domain.order.dto.packaging;
 
+import com.nhnacademy.illuwa.domain.order.repository.PackagingRepository;
+import com.querydsl.core.annotations.QueryProjection;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,11 +12,18 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Builder
 public class PackagingResponseDto {
     private long packagingId;
     private String packagingName;
     private BigDecimal packagingPrice;
+
+    @QueryProjection
+    public PackagingResponseDto(long packagingId, String packagingName, BigDecimal packagingPrice) {
+        this.packagingId = packagingId;
+        this.packagingName = packagingName;
+        this.packagingPrice = packagingPrice;
+    }
 }
 
 // order 서버 -> 프론트 (포장지 조회 정보 전송)
