8e01280127a06ada0c545de0e3b5f7a27708b138
pear_c <zxcv9676@naver.com>
2025-06-23 11:12:55 +0900

    feat: ci/cd 파이프라인 분리

--- src/test/java/com/nhnacademy/illuwa/domain/coupon/service/CouponPolicyServiceImplTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/coupon/service/CouponPolicyServiceImplTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -25,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
+@ActiveProfiles("db")
 class CouponPolicyServiceImplTest {
 
     @Autowired
