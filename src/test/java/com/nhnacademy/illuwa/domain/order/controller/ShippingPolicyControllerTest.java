898b9f425392cc57bbaec46bb15c9c8aac076e88
koreanguyyyy <park6049612@naver.com>
2025-06-24 16:59:32 +0900

    test: 배송 정책 컨트롤러 테스트코드 수정

--- src/test/java/com/nhnacademy/illuwa/domain/order/controller/ShippingPolicyControllerTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/order/controller/ShippingPolicyControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.math.BigDecimal;
 import java.util.List;
