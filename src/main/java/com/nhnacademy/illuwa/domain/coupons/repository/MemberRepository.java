589743aa39da31675bb8d695a2bcbcc4f0e39527
JJung0 <junghwan__@naver.com>
2025-06-26 16:13:42 +0900

    중간저장

--- src/main/java/com/nhnacademy/illuwa/domain/coupons/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/illuwa/domain/coupons/repository/MemberRepository.java
@@ -2,12 +2,17 @@ package com.nhnacademy.illuwa.domain.coupons.repository;
 
 import com.nhnacademy.illuwa.domain.coupons.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByEmail(String email);
+
+    @Query("SELECT m FROM Member m WHERE MONTH(m.birth) = :month")
+    List<Member> findMemberByBirthDay(short birthDay);
 
     Optional<Member> findMemberById(Long id);
 }
