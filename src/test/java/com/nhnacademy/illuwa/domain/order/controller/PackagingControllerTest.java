0b0eae2f9bc20e14edb199700dd63c1a0bdf3825
pear_c <zxcv9676@naver.com>
2025-06-27 15:44:33 +0900

    fix: config 설정 추가, 스프링부트 버전 변경 (3.3.12)

--- src/test/java/com/nhnacademy/illuwa/domain/order/controller/PackagingControllerTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/order/controller/PackagingControllerTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -30,7 +30,7 @@ class PackagingControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @MockBean
     private PackagingService packagingService;
 
     private PackagingCreateRequestDto requestDto;
