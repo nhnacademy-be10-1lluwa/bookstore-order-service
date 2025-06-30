baaa3867b8825ea2dd04226c4e1724a87d4f7837
koreanguyyyy <park6049612@naver.com>
2025-06-30 17:25:16 +0900

    feat: userId 어노테이션 코드 추가

---
+++ src/main/java/com/nhnacademy/illuwa/common/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.illuwa.common.config;
+
+import com.nhnacademy.illuwa.common.resolver.CurrentUserIdResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final CurrentUserIdResolver currentUserIdResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserIdResolver);
+    }
+}
