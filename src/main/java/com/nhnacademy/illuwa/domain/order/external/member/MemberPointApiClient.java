fd477a6306221107f67c481c5d5352468a94d9dd
koreanguyyyy <park6049612@naver.com>
2025-07-01 10:32:51 +0900

    refactor: feignClient API 주소 변경

--- src/main/java/com/nhnacademy/illuwa/domain/order/external/member/MemberPointApiClient.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/external/member/MemberPointApiClient.java
@@ -6,6 +6,7 @@ import com.nhnacademy.illuwa.domain.order.external.member.dto.MemberSavePointDto
 import com.nhnacademy.illuwa.domain.order.external.member.dto.MemberUsedPointDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.math.BigDecimal;
@@ -19,16 +20,16 @@ import java.util.Optional;
 public interface MemberPointApiClient {
 
     // 사용된 포인트 전송
-    @PostMapping(value = "/members/points/use")
-    MemberUsedPointDto sendUsedPointByMemberId(Long memberId, BigDecimal usedPoint);
+    @PostMapping(value = "/members/{memberId}/points/use")
+    MemberUsedPointDto sendUsedPointByMemberId(@PathVariable Long memberId, BigDecimal usedPoint);
 
     // 순수 금액 전송 (상품 구매시, 적립금 적용에 사용)
-    @PostMapping(value = "/members/points/earn")
-    MemberSavePointDto sendTotalPrice(Long memberId, BigDecimal totalPrice);
+    @PostMapping(value = "/members/{memberId}/points/earn")
+    MemberSavePointDto sendTotalPrice(@PathVariable Long memberId, BigDecimal totalPrice);
 
     // 해당 멤버의 포인트 조회
-    @GetMapping(value = "/members/points")
-    Optional<MemberPointDto> getPointByMemberId(Long memberId);
+    @GetMapping(value = "/members/{memberId}/points")
+    Optional<MemberPointDto> getPointByMemberId(@PathVariable Long memberId);
 
     // 멤버들의 3개월간 순수 주문 금액 전송 (주문 상태: confirmed )
     @PostMapping(value = "/members/grades/update")
