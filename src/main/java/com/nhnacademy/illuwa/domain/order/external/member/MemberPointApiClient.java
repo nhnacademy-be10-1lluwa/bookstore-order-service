69719083e2e021a711bdeb68a9fec9fb6d967b24
koreanguyyyy <152121113+koreanguyyyy@users.noreply.github.com>
2025-07-01 09:29:39 +0900

    Feature: #162 external feignClient (#166)
    * feat: userId 어노테이션 코드 추가
    
    * clean up: 필요없는 dto 삭제
    
    * feat: external feignclient 적용

--- src/main/java/com/nhnacademy/illuwa/domain/order/external/member/MemberPointApiClient.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/external/member/MemberPointApiClient.java
@@ -1,28 +1,36 @@
 package com.nhnacademy.illuwa.domain.order.external.member;
 
-
-
 import com.nhnacademy.illuwa.domain.order.external.member.dto.MemberGradeUpdateRequest;
 import com.nhnacademy.illuwa.domain.order.external.member.dto.MemberPointDto;
 import com.nhnacademy.illuwa.domain.order.external.member.dto.MemberSavePointDto;
 import com.nhnacademy.illuwa.domain.order.external.member.dto.MemberUsedPointDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@FeignClient(
+        name = "UserService",
+        url = "${user-api.url}"
+)
 public interface MemberPointApiClient {
 
     // 사용된 포인트 전송
+    @PostMapping(value = "/points/use")
     MemberUsedPointDto sendUsedPointByMemberId(Long memberId, BigDecimal usedPoint);
 
     // 순수 금액 전송 (상품 구매시, 적립금 적용에 사용)
+    @PostMapping(value = "/points/earn")
     MemberSavePointDto sendTotalPrice(Long memberId, BigDecimal totalPrice);
 
     // 해당 멤버의 포인트 조회
+    @GetMapping(value = "/points")
     Optional<MemberPointDto> getPointByMemberId(Long memberId);
 
     // 멤버들의 3개월간 순수 주문 금액 전송 (주문 상태: confirmed )
+    @PostMapping(value = "/members/grades/update")
     List<MemberGradeUpdateRequest> sendNetOrderAmount();
-
 }
