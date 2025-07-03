00f388da463d54eba14be9ee635cb9be795fe000
koreanguyyyy <park6049612@naver.com>
2025-07-03 15:21:16 +0900

    feat: 장바구니 주문 컨트롤러 코드 추가

--- src/main/java/com/nhnacademy/illuwa/domain/order/controller/guest/GuestOrderController.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/controller/guest/GuestOrderController.java
@@ -1,8 +1,9 @@
 package com.nhnacademy.illuwa.domain.order.controller.guest;
 
 import com.nhnacademy.illuwa.domain.order.dto.order.*;
+import com.nhnacademy.illuwa.domain.order.dto.order.guest.GuestOrderInitFromCartResponseDto;
+import com.nhnacademy.illuwa.domain.order.dto.order.guest.GuestOrderRequest;
 import com.nhnacademy.illuwa.domain.order.entity.Order;
-import com.nhnacademy.illuwa.domain.order.exception.common.NotFoundException;
 import com.nhnacademy.illuwa.domain.order.service.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +18,25 @@ public class GuestOrderController {
     private final OrderService orderService;
 
     @GetMapping("/init-from-cart")
-    public ResponseEntity<GuestOrderInitResponseDto> getOrderInitFromCart(
+    public ResponseEntity<GuestOrderInitFromCartResponseDto> getOrderInitFromCart(
             @CookieValue(value = "cartId", required = false) Long cartId
     ) {
         if (cartId == null) {
             throw new IllegalArgumentException("장바구니 정보가 없습니다.");
         }
-        GuestOrderInitResponseDto response = orderService.getGuestOrderInitFromCartData(cartId);
+        GuestOrderInitFromCartResponseDto response = orderService.getGuestOrderInitFromCartData(cartId);
         return ResponseEntity.ok(response);
     }
+
+    // 비회원 장바구니 주문
+    @PostMapping("/submit")
+    public ResponseEntity<OrderCreateResponseDto> guestOrderRequest(@RequestBody GuestOrderRequest guestOrderRequest) {
+        Order order = orderService.guestCreateOrderFromCartWithItems(guestOrderRequest);
+        OrderCreateResponseDto response = OrderCreateResponseDto.fromEntity(order);
+        return ResponseEntity.ok(response);
+    }
+
+
 
     // 비회원 주문내역 확인
     @GetMapping("/orders/{orderNumber}")
@@ -34,12 +45,4 @@ public class GuestOrderController {
         return ResponseEntity.ok(response);
     }
 
-    // 비회원 주문하기
-    @PostMapping("/orders")
-    public ResponseEntity<OrderCreateResponseDto> sendOrderRequest(@RequestBody OrderCreateRequestDto orderCreateRequestDto) {
-        // todo guestId 넘기는 로직 만드릭
-        Order order = orderService.createOrderWithItems(orderCreateRequestDto);
-        OrderCreateResponseDto response = OrderCreateResponseDto.fromEntity(order);
-        return ResponseEntity.ok(response);
-    }
 }
