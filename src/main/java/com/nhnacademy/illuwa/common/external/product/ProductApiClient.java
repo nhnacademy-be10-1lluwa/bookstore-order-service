c53e1c527fd71f0793abc3a1df221f24a1218bdd
JJung0 <junghwan__@naver.com>
2025-07-10 17:21:42 +0900

    feginclient api 경로 추가

--- src/main/java/com/nhnacademy/illuwa/common/external/product/ProductApiClient.java
+++ src/main/java/com/nhnacademy/illuwa/common/external/product/ProductApiClient.java
@@ -20,28 +20,28 @@ public interface ProductApiClient {
 
 
     // 책 가격 가져오기
-    @GetMapping(value = "/books", params = "bookId")
+    @GetMapping(value = "/api/books", params = "bookId")
     Optional<BookPriceDto> getBookPriceByBookId(@RequestParam("bookId") Long bookId);
 
     // 책 정보 가져오기
-    @GetMapping("/books/{bookId}")
+    @GetMapping("/api/books/{bookId}")
     Optional<BookDto> getBookById(@PathVariable("bookId") Long bookId);
 
 
     // 카테고리 가져오기
-    @GetMapping("/categories/{categoryId}")
+    @GetMapping("/api/categories/{categoryId}")
     CategoryDto getCategoryById(@PathVariable("categoryId") Long categoryId);
 
 
     // 카트 조회 (Member)
-    @GetMapping(value = "/members/{memberId}/cart")
+    @GetMapping(value = "/api/members/{memberId}/cart")
     Optional<CreateOrderFromCartRequest> getCart(@PathVariable Long memberId);
 
     // 카트 조회 (Guest)
-    @GetMapping(value = "/guest/cart/{cartId}")
+    @GetMapping(value = "/api/guest/cart/{cartId}")
     Optional<CreateOrderFromCartRequest> getGuestCart(@PathVariable Long cartId);
 
     // 카트 비우기 요청
-    @DeleteMapping(value = "/members/{memberId}/cart")
+    @DeleteMapping(value = "/api/members/{memberId}/cart")
     void sendRemoveCartItem(@PathVariable Long memberId);
 }
