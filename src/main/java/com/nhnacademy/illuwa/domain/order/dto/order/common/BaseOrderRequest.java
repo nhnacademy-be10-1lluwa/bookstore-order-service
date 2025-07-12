cb7d17e56fd39afb2e463c5c6744e038f41df4c2
koreanguyyyy <park6049612@naver.com>
2025-07-12 18:49:59 +0900

    feat: dto 수정 및 추가

--- src/main/java/com/nhnacademy/illuwa/domain/order/dto/order/common/BaseOrderRequest.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/dto/order/common/BaseOrderRequest.java
@@ -6,12 +6,14 @@ import lombok.experimental.SuperBuilder;
 import java.time.LocalDate;
 
 @SuperBuilder
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public abstract class BaseOrderRequest {
     private LocalDate deliveryDate;
     private String recipientName;
     private String recipientContact;
+    private String postCode;
     private String readAddress;
     private String detailAddress;
 }
