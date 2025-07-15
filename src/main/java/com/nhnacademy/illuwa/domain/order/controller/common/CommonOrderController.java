8d768fb9ec5948858e1b7bea31714b35393364c0
koreanguyyyy <park6049612@naver.com>
2025-07-15 17:35:49 +0900

    feat: 회원 등급 재조정 로직 변경

--- src/main/java/com/nhnacademy/illuwa/domain/order/controller/common/CommonOrderController.java
+++ src/main/java/com/nhnacademy/illuwa/domain/order/controller/common/CommonOrderController.java
@@ -1,22 +1,35 @@
 package com.nhnacademy.illuwa.domain.order.controller.common;
 
 
+import com.nhnacademy.illuwa.domain.order.service.MemberGradeService;
 import com.nhnacademy.illuwa.domain.order.service.OrderService;
-import jakarta.ws.rs.Path;
+import com.nhnacademy.illuwa.domain.order.util.scheduler.MonthlyNetOrderAmountScheduler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/order/common")
 public class CommonOrderController {
 
     private final OrderService orderService;
+    private final MonthlyNetOrderAmountScheduler scheduler;
+    private final MemberGradeService memberGradeService;
 
     @PostMapping("/payment-success/{orderNumber}")
     public ResponseEntity<Void> updateOrderStatus(@PathVariable("orderNumber") String orderNumber) {
         orderService.updateOrderPaymentByOrderNumber(orderNumber);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/send-net-order-amount")
+    public ResponseEntity<Void> triggerSchedulerManually() {
+        int count = memberGradeService.sendMonthlyNetOrderAmount();
+        log.info("수동으로 원간 순주문 금액 전송 와료 - {}건", count);
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/nhnacademy/illuwa/domain/order/service/impl/MemberGradeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.illuwa.domain.order.service.impl;
+
+import com.nhnacademy.illuwa.common.external.user.UserApiClient;
+import com.nhnacademy.illuwa.common.external.user.dto.MemberGradeUpdateRequest;
+import com.nhnacademy.illuwa.domain.order.repository.OrderRepository;
+import com.nhnacademy.illuwa.domain.order.service.MemberGradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberGradeServiceImpl implements MemberGradeService {
+    private final OrderRepository orderRepository;
+    private final UserApiClient userApiClient;
+
+    @Override
+    public int sendMonthlyNetOrderAmount() {
+        List<MemberGradeUpdateRequest> reqs = orderRepository.buildMemberGradeUpdateRequest();
+        return userApiClient.sendNetOrderAmount(reqs).size();
+    }
+}
