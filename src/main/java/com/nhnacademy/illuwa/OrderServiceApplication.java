660556c88f925e7a3257968695a81d2d133123df
JJung0 <junghwan__@naver.com>
2025-07-16 09:41:39 +0900

    rabbitMQ재활성화

--- src/main/java/com/nhnacademy/illuwa/OrderServiceApplication.java
+++ src/main/java/com/nhnacademy/illuwa/OrderServiceApplication.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.illuwa;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
+@EnableRabbit
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
